642b4ed34fb5c4bcb3ea4e51b90b8590750c26ea
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-19 16:12:11 +0100

    Converted transformer to use ES6 class syntax.

--- jetblack-converters/lib/transformer.js
+++ jetblack-converters/lib/transformer.js
@@ -1,24 +1,45 @@
-﻿function Transformer(converter, targetConverter, convertTo, convertFrom) {
-    this.converter = converter;
-    this.targetConverter = targetConverter;
-    this.convertTo = convertTo;
-    this.convertFrom = convertFrom;
-}
+﻿"use strict";
 
-Transformer.prototype.transform = function (value, from, to, scalar) {
-    if (from.domain == this.converter.domain) {
-        value = from.convert(value, this.converter);
-        value = this.convertTo(value, scalar);
-        return this.targetConverter.convert(value, to);
-    } else if (to.domain == this.converter.domain) {
-        value = from.convert(value, this.targetConverter);
-        value = this.convertFrom(value, scalar);
-        return this.converter.convert(value, to);
+class Transformer {
+
+    constructor(converter, targetConverter, convertTo, convertFrom) {
+        this._converter = converter;
+        this._targetConverter = targetConverter;
+        this._convertTo = convertTo;
+        this._convertFrom = convertFrom;
     }
-};
 
-Transformer.prototype.toString = function () {
-    return this.converter.domain + " to " + this.targetConverter.domain;
+    get converter() {
+        return this._converter;
+    }
+
+    get targetConverter() {
+        return this._targetConverter;
+    }
+
+    get convertTo() {
+        return this._convertTo;
+    }
+
+    get convertFrom() {
+        return this._convertFrom;
+    }
+
+    transform(value, from, to, scalar) {
+        if (from.domain == this._converter.domain) {
+            value = from.convert(value, this._converter);
+            value = this._convertTo(value, scalar);
+            return this._targetConverter.convert(value, to);
+        } else if (to.domain == this._converter.domain) {
+            value = from.convert(value, this.targetConverter);
+            value = this._convertFrom(value, scalar);
+            return this._converter.convert(value, to);
+        }
+    }
+
+    toString() {
+        return this._converter.domain + " to " + this._targetConverter.domain;
+    }
 }
 
 module.exports = Transformer;
