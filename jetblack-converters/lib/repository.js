72c4726c33fdcd96ba1ffd0aa104c53063b9a196
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-19 16:03:50 +0100

    Converted repository module to use ES6 class syntax.

--- jetblack-converters/lib/repository.js
+++ jetblack-converters/lib/repository.js
@@ -1,21 +1,30 @@
-﻿function TransformerRepository() {
-    this.transformers = [];
-}
+﻿"use strict";
 
-TransformerRepository.prototype.add = function (transformer) {
-    this.transformers.push(transformer);
-    return transformer;
-};
+class TransformerRepository {
 
-TransformerRepository.prototype.find = function (domain, targetDomain) {
-    for (var index = 0; index < this.transformers.length; ++index) {
-        var transformer = this.transformers[index];
-        if ((domain == transformer.converter.domain && targetDomain == transformer.targetConverter.domain)
-            || (domain == transformer.targetConverter.domain && targetDomain == transformer.converter.domain)) {
-            return transformer;
+    constructor() {
+        this._transformers = [];
+    }
+
+    get transformers() {
+        return this._transformers;
+    }
+
+    add(transformer) {
+        this._transformers.push(transformer);
+        return transformer;
+    }
+
+    find(domain, targetDomain) {
+        for (var index = 0; index < this._transformers.length; ++index) {
+            var transformer = this._transformers[index];
+            if ((domain == transformer.converter.domain && targetDomain == transformer.targetConverter.domain)
+                || (domain == transformer.targetConverter.domain && targetDomain == transformer.converter.domain)) {
+                return transformer;
+            }
         }
     }
-};
+}
 
 var filter = function (candidates, criterium, getter) {
     if (criterium) {
@@ -31,63 +40,82 @@ var filter = function (candidates, criterium, getter) {
     }
 };
 
-function ConverterRepository(defaultClassification, defaultAuthority) {
+class ConverterRepository {
 
-    this.defaultClassification = defaultClassification;
-    this.defaultAuthority = defaultAuthority;
+    constructor(defaultClassification, defaultAuthority) {
 
-    this.converters = [];
-    this.transformers = new TransformerRepository();
+        this._defaultClassification = defaultClassification;
+        this._defaultAuthority = defaultAuthority;
+
+        this._converters = [];
+        this._transformers = new TransformerRepository();
+    }
+
+    get defaultClassification() {
+        return this._defaultClassification;
+    }
+
+    get defaultAuthority() {
+        return this._defaultAuthority;
+    }
+
+    get converters() {
+        return this._converters;
+    }
+
+    get transformers() {
+        return this._transformers;
+    }
+
+    add(converter) {
+        this._converters.push(converter);
+        return converter;
+    }
+
+    find(criteria) {
+        var possibles = this.findAll(criteria);
+
+        var filtered;
+
+        if (possibles.length > 1) {
+            filtered = filter(possibles, this._defaultClassification, function (converter) { return converter.system; });
+            if (filtered.length > 0) {
+                possibles = filtered;
+            }
+        }
+
+        if (possibles.length > 1) {
+            filtered = filter(possibles, this._defaultAuthority, function (converter) { return converter.authority; });
+            if (filtered.length > 0) {
+                possibles = filtered;
+            }
+        }
+
+        return possibles[0];
+    }
+
+    findAll(criteria) {
+
+        var possibles = this._converters;
+        possibles = filter(possibles, criteria.domain, function (converter) { return converter.domain; });
+        possibles = filter(possibles, criteria.system, function (converter) { return converter.system; });
+        possibles = filter(possibles, criteria.authority, function (converter) { return converter.authority; });
+        possibles = filter(possibles, criteria.symbol, function (converter) { return converter.symbol; });
+        possibles = filter(possibles, criteria.name, function (converter) { return converter.name; });
+
+        return possibles;
+    }
+
+    convert(converter, value, targetConverter) {
+        if (converter.domain == targetConverter.domain) {
+            return converter.convert(value, targetConverter);
+        } else {
+            var transformer = this._transformers.find(converter.domain, targetConverter.domain);
+            if (transformer) {
+                return transformer.transform(value, converter, targetConverter);
+            }
+        }
+    }
 }
-
-ConverterRepository.prototype.add = function (converter) {
-    this.converters.push(converter);
-    return converter;
-};
-
-ConverterRepository.prototype.find = function (criteria) {
-    var possibles = this.findAll(criteria);
-
-    var filtered;
-
-    if (possibles.length > 1) {
-        filtered = filter(possibles, this.defaultClassification, function (converter) { return converter.system; });
-        if (filtered.length > 0) {
-            possibles = filtered;
-        }
-    }
-
-    if (possibles.length > 1) {
-        filtered = filter(possibles, this.defaultAuthority, function (converter) { return converter.authority; });
-        if (filtered.length > 0) {
-            possibles = filtered;
-        }
-    }
-
-    return possibles[0];
-};
-
-ConverterRepository.prototype.findAll = function (criteria) {
-
-    var possibles = this.converters;
-    possibles = filter(possibles, criteria.domain, function (converter) { return converter.domain; });
-    possibles = filter(possibles, criteria.system, function (converter) { return converter.system; });
-    possibles = filter(possibles, criteria.authority, function (converter) { return converter.authority; });
-    possibles = filter(possibles, criteria.symbol, function (converter) { return converter.symbol; });
-    possibles = filter(possibles, criteria.name, function (converter) { return converter.name; });
-
-    return possibles;
-};
-
-ConverterRepository.prototype.convert = function (converter, value, targetConverter) {
-    if (converter.domain == targetConverter.domain) {
-        return converter.convert(value, targetConverter);
-    } else {
-        var transformer = this.transformers.find(converter.domain, targetConverter.domain);
-        if (transformer) {
-            return transformer.transform(value, converter, targetConverter);
-        }
-    }
-};
 
 module.exports = new ConverterRepository();
