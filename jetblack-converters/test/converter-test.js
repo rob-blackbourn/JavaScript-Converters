7a3d17f6e842547a8fbdfc791dcc2ba2cc7427a6
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-19 14:49:19 +0100

    Initial version

---
+++ jetblack-converters/test/converter-test.js
@@ -0,0 +1,33 @@
+﻿var assert = require('assert');
+
+var converters = require('../lib/index.js'),
+    Converter = converters.Converter,
+    repository = converters.repository,
+    numbers = converters.numbers,
+    Fraction = numbers.Fraction,
+    Real = numbers.Real;
+
+
+describe('converter', function () {
+
+    it('Should convert inches to millimeters', function () {
+
+        var millimeterConverter = repository.find({ name: 'millimeter' });
+        var inchConverter = repository.find({ system: 'imperial', name: 'inch' });
+
+        var result = inchConverter.convert(new Fraction(1, 1), millimeterConverter);
+
+        assert.equal(result.valueOf(), 25.4, "There are 25.4 millimeters in an inch.");
+    });
+
+    it('Should convert millimeters to inches', function () {
+
+        var millimeterConverter = repository.find({ name: 'millimeter' });
+        var inchConverter = repository.find({ system: 'imperial', name: 'inch' });
+
+        var result = millimeterConverter.convert(new Real(25.4), inchConverter);
+
+        assert.equal(result.valueOf(), 1, "There are 25.4 millimeters in an inch.");
+    });
+
+})
---
+++ jetblack-converters/test/numbers-test.js
@@ -0,0 +1,73 @@
+﻿var assert = require('assert');
+
+var numbers = require('../lib/numbers.js'),
+    Fraction = numbers.Fraction,
+    Real = numbers.Real;
+
+describe('numbers', function () {
+
+    it('Should construct a simple fraction', function () {
+
+        var f1 = new Fraction(1, 2);
+        assert.equal(f1.numerator, 1, "The numerator should be 1");
+        assert.equal(f1.denominator, 2, "The denominator should be 2");
+    })
+
+    it('Should simplify.', function () {
+
+        var f1 = new Fraction(2, 4);
+        assert.equal(f1.numerator, 1, "The numerator should be 1");
+        assert.equal(f1.denominator, 2, "The denominator should be 2");
+    })
+
+    it('should return NaN', function () {
+        var f = new Fraction(1, 0);
+        assert.ok(f.isNaN());
+    });
+
+    it('should return construct fractions of value 1', function () {
+        for (var i = 1; i <= 10; ++i) {
+            var f = new Fraction(i, i);
+            assert.equal(1, f.numerator);
+            assert.equal(1, f.denominator);
+        }
+    });
+
+    it('should normalise negatives', function () {
+        var f1 = new Fraction(-1, -2);
+        assert.equal(f1.numerator, 1, "If both the numerator and denominator are negative the numerator should become positive.");
+        assert.equal(f1.denominator, 2, "If both the numerator and denominator are negative the denominator should become positive.");
+
+        var f2 = new Fraction(-1, 2);
+        assert.equal(f2.numerator, -1, "If the numerator is negative and the denominator is positive the numerator should stay negative.");
+        assert.equal(f2.denominator, 2, "If the numerator is negative and the denominator is positive the denominator should stay postitive.");
+
+        var f3 = new Fraction(1, -2);
+        assert.equal(f3.numerator, -1, "If the numerator is positive and the denominator is negative the numerator should become negative.");
+        assert.equal(f3.denominator, 2, "If the numerator is positive and the denominator is negative the numerator should become postive.");
+    });
+
+    it('should print and parse', function () {
+
+        var a1 = new Fraction(3, 2);
+        var a2 = a1.toString();
+        var a3 = Fraction.parse(a2);
+        var a4 = a3.toString();
+        assert.ok(a1.eq(a3), "Should print and parse back a top heavy fraction.");
+        assert.equal(a2, a4, "The print representation of a top heavy fraction shouldn't change.");
+
+        var b1 = new Fraction(1, 1);
+        var b2 = b1.toString();
+        var b3 = Fraction.parse(b2);
+        var b4 = b3.toString();
+        assert.ok(b1.eq(b3), "Should print and parse back a whole number fraction.");
+        assert.equal(b2, b4, "The print representation of a whole number fraction shouldn't change.");
+
+        var c1 = new Fraction(1, 2);
+        var c2 = c1.toString();
+        var c3 = Fraction.parse(c2);
+        var c4 = c3.toString();
+        assert.ok(c1.eq(c3), "Should print and parse back a fraction.");
+        assert.equal(c2, c4, "The print representation of a fraction shouldn't change.");
+    });
+})
