7547739878c8e99cce3aa6a0184ebe53d66b714a
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-19 15:56:52 +0100

    Convert converter to ES6 class syntax.

--- jetblack-converters/lib/converter.js
+++ jetblack-converters/lib/converter.js
@@ -1,59 +1,96 @@
-﻿var numbers = require('./numbers');
+﻿"use strict";
 
-function Converter(domain, system, authority, symbol, name, targetConverter, toTarget, fromTarget) {
-    this.domain = domain;
-    this.system = system;
-    this.authority = authority;
-    this.symbol = symbol;
-    this.name = name;
-    this.targetConverter = targetConverter;
-    this.toTarget = toTarget;
-    this.fromTarget = fromTarget;
-}
+var Real = require('./numbers').Real;
 
-Converter.prototype.toString = function () {
-    return "domain=" + this.domain + ",system=" + this.system + ",authority=" + this.authority + ",symbol=" + this.symbol + ",name=" + this.name;
-};
+class Converter {
 
-Converter.prototype.convert = function (value, to) {
-
-    if (!(value instanceof numbers.Real)) {
-        value = new numbers.Real(value);
+    constructor(domain, system, authority, symbol, name, targetConverter, toTarget, fromTarget) {
+        this._domain = domain;
+        this._system = system;
+        this._authority = authority;
+        this._symbol = symbol;
+        this._name = name;
+        this._targetConverter = targetConverter;
+        this._toTarget = toTarget;
+        this._fromTarget = fromTarget;
     }
 
-    var toConverters = [];
-    var toConverter = to;
-    while (toConverter.fromTarget) {
-        toConverters.push(toConverter);
-        toConverter = toConverter.targetConverter;
+    get domain() {
+        return this._domain;
     }
 
-    var from = this;
-    while (from.toTarget) {
+    get system() {
+        return this._system;
+    }
 
-        if (from == to) {
-            return value;
+    get authority() {
+        return this._authority;
+    }
+
+    get symbol() {
+        return this._symbol;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get targetConverter() {
+        return this._targetConverter;
+    }
+
+    get toTarget() {
+        return this._toTarget;
+    }
+
+    get fromTarget() {
+        return this._fromTarget;
+    }
+
+    toString() {
+        return "domain=" + this._domain + ",system=" + this._system + ",authority=" + this._authority + ",symbol=" + this._symbol + ",name=" + this._name;
+    }
+
+    convert(value, to) {
+
+        if (!(value instanceof Real)) {
+            value = new Real(value);
         }
 
-        value = from.toTarget(value);
+        var toConverters = [];
+        var toConverter = to;
+        while (toConverter.fromTarget) {
+            toConverters.push(toConverter);
+            toConverter = toConverter.targetConverter;
+        }
 
-        var index = toConverters.indexOf(from);
+        var from = this;
+        while (from.toTarget) {
 
-        if (index == -1) {
-            from = from.targetConverter;
-        } else {
-            for (; index >= 0; --index) {
-                value = toConverters[index].fromTarget(value);
+            if (from == to) {
+                return value;
             }
-            return value;
+
+            value = from.toTarget(value);
+
+            var index = toConverters.indexOf(from);
+
+            if (index == -1) {
+                from = from.targetConverter;
+            } else {
+                for (; index >= 0; --index) {
+                    value = toConverters[index].fromTarget(value);
+                }
+                return value;
+            }
         }
-    }
 
-    while (toConverters.length > 0) {
-        value = toConverters.pop().fromTarget(value);
-    }
+        while (toConverters.length > 0) {
+            value = toConverters.pop().fromTarget(value);
+        }
 
-    return value;
-};
+        return value;
+    }
+}
 
 module.exports = Converter;
