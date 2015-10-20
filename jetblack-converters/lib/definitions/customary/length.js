5e3404da88bf08a6a0726c86ece78117b7404e68
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-20 13:26:09 +0100

    Tidy up imperial lengths

--- jetblack-converters/lib/definitions/customary/length.js
+++ jetblack-converters/lib/definitions/customary/length.js
@@ -1,60 +1,69 @@
 ﻿var Converter = require('../../converter');
-var numbers = require('../../numbers');
-var Real = numbers.Real;
+
+var numbers = require('../../numbers'),
+    Fraction = numbers.Fraction,
+    Real = numbers.Real;
 
 module.exports = function (repository) {
 
-    var thouScalar = new Real(12000);
-    var inchScalar = new Real(12);
-    var yardScalar = new Real(3);
-    var chainScalar = new Real(66);
-    var furlongScalar = new Real(660);
-    var mileScalar = new Real(5280);
-    var leagueScalar = new Real(15840);
-
     var meterConverter = repository.find({ name: 'meter' });
+    var meterScalar = new Real(new Fraction(3048, 10000));
     var feetConverter = repository.add(new Converter("length", "customary", "USA", "ft", "feet", meterConverter,
         function (value) {
-            return value.mul(0.3048);
+            return value.mul(meterScalar);
         }, function (value) {
-            return value.div(0.3048);
+            return value.div(meterScalar);
         }));
+
+    var thouScalar = new Real(12000);
     repository.add(new Converter("length", "customary", "USA", "th", "thou", feetConverter,
         function (value) {
             return value.div(thouScalar);
         }, function (value) {
             return value.mul(thouScalar);
         }));
+
+    var inchScalar = new Real(12);
     repository.add(new Converter("length", "customary", "USA", "in", "inch", feetConverter,
         function (value) {
             return value.div(inchScalar);
         }, function (value) {
             return value.mul(inchScalar);
         }));
+
+    var yardScalar = new Real(3);
     repository.add(new Converter("length", "customary", "USA", "yd", "yard", feetConverter,
         function (value) {
             return value.mul(yardScalar);
         }, function (value) {
             return value.div(yardScalar);
         }));
+
+    var chainScalar = new Real(66);
     repository.add(new Converter("length", "customary", "USA", "ch", "chain", feetConverter,
         function (value) {
             return value.mul(chainScalar);
         }, function (value) {
             return value.div(chainScalar);
         }));
+
+    var furlongScalar = new Real(660);
     repository.add(new Converter("length", "customary", "USA", "fur", "furlong", feetConverter,
         function (value) {
             return value.mul(furlongScalar);
         }, function (value) {
             return value.div(furlongScalar);
         }));
+
+    var mileScalar = new Real(5280);
     repository.add(new Converter("length", "customary", "USA", "mi", "mile", feetConverter,
         function (value) {
             return value.mul(mileScalar);
         }, function (value) {
             return value.div(mileScalar);
         }));
+
+    var leagueScalar = new Real(15840);
     repository.add(new Converter("length", "customary", "USA", "lea", "league", feetConverter,
         function (value) {
             return value.mul(leagueScalar);
--- jetblack-converters/lib/definitions/imperial/length.js
+++ jetblack-converters/lib/definitions/imperial/length.js
@@ -1,64 +1,73 @@
 ﻿var Converter = require('../../converter');
-var numbers = require('../../numbers');
-var Real = numbers.Real;
+
+var numbers = require('../../numbers'),
+    Fraction = numbers.Fraction,
+    Real = numbers.Real;
 
 module.exports = function (repository) {
 
-    var thouScalar = new Real(12000);
-    var inchScalar = new Real(12);
-    var yardScalar = new Real(3);
-    var chainScalar = new Real(66);
-    var furlongScalar = new Real(660);
-    var mileScalar = new Real(5280);
-    var leagueScalar = new Real(15840);
-
     var meterConverter = repository.find({ name: 'meter' });
+    var meterScalar = new Real(new Fraction(3048, 10000));
     var feetConverter = repository.add(new Converter("length", "imperial", "UK", "ft", "feet", meterConverter,
-        function (value) {
-            return value.mul(0.3048);
-        }, function (value) {
-            return value.div(0.3048);
+        function (feet) {
+            return feet.mul(meterScalar);
+        }, function (meter) {
+            return meter.div(meterScalar);
         }));
+
+    var thouScalar = new Real(12000);
     repository.add(new Converter("length", "imperial", "UK", "th", "thou", feetConverter,
-        function (value) {
-            return value.div(thouScalar);
-        }, function (value) {
-            return value.mul(thouScalar);
+        function (feet) {
+            return feet.div(thouScalar);
+        }, function (thou) {
+            return thou.mul(thouScalar);
         }));
+
+    var inchScalar = new Real(12);
     repository.add(new Converter("length", "imperial", "UK", "in", "inch", feetConverter,
-        function (value) {
-            return value.div(inchScalar);
-        }, function (value) {
-            return value.mul(inchScalar);
+        function (feet) {
+            return feet.div(inchScalar);
+        }, function (inch) {
+            return inch.mul(inchScalar);
         }));
+
+    var yardScalar = new Real(3);
     repository.add(new Converter("length", "imperial", "UK", "yd", "yard", feetConverter,
-        function (value) {
-            return value.mul(yardScalar);
-        }, function (value) {
-            return value.div(yardScalar);
+        function (feet) {
+            return feet.mul(yardScalar);
+        }, function (yard) {
+            return yard.div(yardScalar);
         }));
+
+    var chainScalar = new Real(66);
     repository.add(new Converter("length", "imperial", "UK", "ch", "chain", feetConverter,
-        function (value) {
-            return value.mul(chainScalar);
-        }, function (value) {
-            return value.div(chainScalar);
+        function (feet) {
+            return feet.mul(chainScalar);
+        }, function (chain) {
+            return chain.div(chainScalar);
         }));
+
+    var furlongScalar = new Real(660);
     repository.add(new Converter("length", "imperial", "UK", "fur", "furlong", feetConverter,
-        function (value) {
-            return value.mul(furlongScalar);
-        }, function (value) {
-            return value.div(furlongScalar);
+        function (feet) {
+            return feet.mul(furlongScalar);
+        }, function (furlong) {
+            return furlong.div(furlongScalar);
         }));
+
+    var mileScalar = new Real(5280);
     repository.add(new Converter("length", "imperial", "UK", "mi", "mile", feetConverter,
-        function (value) {
-            return value.mul(mileScalar);
-        }, function (value) {
-            return value.div(mileScalar);
+        function (feet) {
+            return feet.mul(mileScalar);
+        }, function (mile) {
+            return mile.div(mileScalar);
         }));
+
+    var leagueScalar = new Real(15840);
     repository.add(new Converter("length", "imperial", "UK", "lea", "league", feetConverter,
-        function (value) {
-            return value.mul(leagueScalar);
-        }, function (value) {
-            return value.div(leagueScalar);
+        function (feet) {
+            return feet.mul(leagueScalar);
+        }, function (league) {
+            return league.div(leagueScalar);
         }));
 };
